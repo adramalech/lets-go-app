d2f14a3b499cff3693feecf79d9d08ad1b8e25b5
Jonathan Throne <jthrone@skyslope.com>
2020-08-24 17:23:55 -0700

    Added in mysql db connection to a local docker container image.

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+    "database/sql"
     "flag"
     "log"
     "net/http"
     "os"
+
+    _ "github.com/go-sql-driver/mysql"
 )
 
 type Config struct {
@@ -15,6 +18,8 @@ type Config struct {
 func main() {
     cfg := new(Config)
     
+    dsn := flag.String("dsn", "root:password12345@/snippetbox?parseTime=true", "MySQL data source name")
+
     app := &application{
         infoLog: log.New(os.Stdout, "INFO\t", log.Ldate|log.LUTC),
         errorLog: log.New(os.Stderr, "ERROR\t", log.Ldate|log.LUTC|log.Llongfile),
@@ -24,6 +29,14 @@ func main() {
     flag.StringVar(&cfg.StaticDir, "static-dir", "./ui-static", "Path to static assets")
 
     flag.Parse()
+
+    db, err := openDB(*dsn)
+    
+    if err != nil {
+        app.errorLog.Fatal(err)
+    }
+
+    defer db.Close()
 
     mux := app.routes(cfg.StaticDir)
 
@@ -37,7 +50,23 @@ func main() {
 
     app.infoLog.Printf("Starting server on %s with pid %d\n", cfg.Addr, pid)
 
-    err := srv.ListenAndServe()
+    err = srv.ListenAndServe()
 
     app.errorLog.Fatal(err)
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+    db, err := sql.Open("mysql", dsn)
+    
+    if err != nil {
+        return nil, err
+    }
+    
+    err = db.Ping()
+
+    if err != nil {
+        return nil, err
+    }
+
+    return db, nil
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/adramalech/lets-go-app/snippetbox
 
 go 1.15
+
+require github.com/go-sql-driver/mysql v1.5.0 // indirect
