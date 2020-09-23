31c98a19666692a846ee604b1439dba27b6b1fb3
Jonathan Throne <jthrone@skyslope.com>
2020-09-23 15:24:49 -0700

    Finished chapter 7 adding in gorilla/mux.

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -33,7 +33,7 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
     ctx := r.Context()
     
-    id, err := strconv.Atoi(r.URL.Query().Get("id"))
+    id, err := strconv.Atoi(r.URL.Query().Get(":id"))
 
     if err != nil || id < 1 {
         app.log.Errorf("Id is not a correct value %d\n", id)
@@ -81,5 +81,9 @@ func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
         return
     }
  
-    http.Redirect(w, r, fmt.Sprintf("/snippet?id=%d", id), http.StatusSeeOther)
+    http.Redirect(w, r, fmt.Sprintf("/snippet/%d", id), http.StatusSeeOther)
+}
+
+func (app *application) createSnippetForm(w http.ResponseWriter, r *http.Request) {
+    w.Write([]byte("Create a new snippet..."))
 }
